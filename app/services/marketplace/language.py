b6de7246e39571532d13a138fa0e47c80389e771
Alex <chedwick21@gmail.com>
2025-12-27 16:59:03 +0000

    marketplace fix lang

--- app/services/marketplace/language.py
+++ app/services/marketplace/language.py
@@ -1,7 +1,7 @@
 from app.models.marketplace.language import Language
 from app.schemas.marketplace.language import LanguageCreate
 
-async def create_language( LanguageCreate) -> Language:
+async def create_language(data: LanguageCreate) -> Language:
     return await Language.create(code=data.code)
 
 async def get_all_languages():
